c54767b647d3583ba20760b2f97882e8fa979a27
Vincent Clyde Burgos <clydeburgos@gmail.com>
2023-08-27 18:13:02 +0800

    Add more updates

---
+++ Codev.JobApplication.Features/Persistence/Job/JobDataService.cs
@@ -0,0 +1,36 @@
+﻿using Codev.JobApplication.Common.Persistence;
+
+namespace Codev.JobApplication.Features.Persistence.Job
+{
+    public class JobDataService : IRepository
+    {
+        public JobDataService()
+        {
+            
+        }
+        public Task<T> GetAsync<T>(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetManyAsync<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertAsync<T>(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update<T>(int id, T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsycn(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
